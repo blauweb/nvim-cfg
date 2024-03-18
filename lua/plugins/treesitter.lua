9cebcc7e4fa2d256cc1af20d14d3c11b68d230b9
blauweb <martin.opara@blauweb.pl>
2024-03-18 20:08:42 +0100

    config update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,47 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "bash",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "query",
-      "regex",
-      "php",
-      "vue",
-      "tsx",
-      "typescript",
-      "vim",
-      "yaml",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "php",
+        "php_only",
+        "vue",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+      indent = { enable = true },
     },
+    config = function(_, opts)
+      ---@class parser_config
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
+        },
+      })
+
+      require("nvim-treesitter.configs").setup(opts)
+    end,
   },
 }
