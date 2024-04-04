3e528159eac441dcbeffe70aaf49fa752d9b54f7
blauweb <martin.opara@blauweb.pl>
2024-04-04 11:22:14 +0200

    remove transparent bg and others

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -3,5 +3,7 @@ return {
   name = "undotree",
   config = function()
     vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle)
+    vim.keymap.set("n", "<leader>U", vim.cmd.UndotreeFocus)
+    vim.g.undotree_SetFocusWhenToggle = 1
   end,
 }
